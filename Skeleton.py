2708bb4f1be2ba50d2135ded0bfe9fe519616a20
smatumla <sheila.matumla@gmail.com>
2026-04-23 13:09:44 -0400

    Deliverable Assignment
    Define Turn_manager class with methods for game flow.

---
+++ Skeleton.py
@@ -0,0 +1,14 @@
+class Turn_manager:
+    def __init__(self, players, player_index):
+        self.players = players
+        self.player_index = player_index
+# Creates class and define initiation with parameters
+        def next_turn():
+            pass
+# Creates method that would change the turns between players.
+        def rest_turn():
+            pass
+ # Creates method that would rest the turn between the players.           
+        def end_game():        
+            pass
+# Creates method that would end the game. 
